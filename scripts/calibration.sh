3a7a2ea9dc9e60dac2b641166787c815533bae10
Pierre Bürki <pierre.burki@he-arc.ch>
2020-07-16 09:58:45 +0200

    Kill the tracker again after calibration

--- scripts/calibration.sh
+++ scripts/calibration.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-# Find the process of the tracker using the camera
-PID=`pgrep the-hole`
+function kill_tracker {
+	PID=`pgrep the-hole`
 
-if [ ! -z $PID ]
-then
-	# Kill tracker (15 = SIGTERM)
-	sudo kill -15 $PID
-fi
+	if [ ! -z $PID ]
+	then
+		# Kill tracker (15 = SIGTERM)
+		sudo kill -15 $PID
+	fi
+}
 
 # Run calibrator
+kill_tracker
 /home/encor/thehole/the-hole-calibrator
 
+# Sometimes, the tracker starts again, but doesn't work (although the process itself doesn't)
+kill_tracker
