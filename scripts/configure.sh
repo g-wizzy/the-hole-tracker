54dbd21cac23748c6343c01765c76344f0c76a1f
Pierre Bürki <pierre.burki@he-arc.ch>
2020-07-16 09:59:31 +0200

    Fix  calibration startup script creation

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -8,17 +8,6 @@ Exec=lxterminal -e "/home/encor/thehole/scripts/startup.sh"
 Terminal=true
 EOF
 
-# set the calibration tool to launch on calib session open
-sudo mkdir -p ~calib/.config/autostart
-cat << EOF | sudo tee ~calib/.config/autostart/theholecalib.desktop > /dev/null
-[Desktop Entry]
-Name=The Hole Calibration
-Type=Application
-Exec=lxterminal -e "/home/encor/thehole/scripts/calibration.sh"
-Terminal=true
-EOF
-sudo chown -R calib:calib ~calib/.config
-
 # Prevent auto-sleep
 sudo systemctl mask sleep.target suspend.target hibernate.target hybrid-sleep.target
 
@@ -33,13 +22,24 @@ sudo adduser --quiet --home /home/calib --shell /bin/bash calib << EOF
 
 Y
 EOF
-
 sudo groupadd thehole
 sudo adduser encor thehole
 sudo adduser calib thehole
 sudo adduser calib sudo
 
+# set the calibration tool to launch on calib session open
+sudo mkdir -p ~calib/.config/autostart
+cat << EOF | sudo tee ~calib/.config/autostart/theholecalib.desktop > /dev/null
+[Desktop Entry]
+Name=The Hole Calibration
+Type=Application
+Exec=lxterminal -e "/home/encor/thehole/scripts/calibration.sh"
+Terminal=true
+EOF
+
 sudo chown -R encor:thehole ~encor/thehole
+chmod g+w ~encor/thehole/data/*.xml
+sudo chown -R calib:calib ~calib/.config
 
 # install xRDP for remote control
 # thanks to https://askubuntu.com/questions/580415/how-to-remote-desktop-from-windows-to-lubuntu
